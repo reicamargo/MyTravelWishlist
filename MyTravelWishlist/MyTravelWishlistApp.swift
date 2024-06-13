3c94a7a320e7f157fa80d65b2cddc8435d3c9057
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-06-13 11:09:53 -0300

    Initial Commit

---
+++ MyTravelWishlist/MyTravelWishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTravelWishlistApp.swift
+//  MyTravelWishlist
+//
+//  Created by Reinaldo Camargo on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTravelWishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
